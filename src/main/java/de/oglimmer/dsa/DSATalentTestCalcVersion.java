4215a23f941bec828cd2b78cfcbcc976e0fe8227
Oli Zimpasser <mail@olikurt.de>
2014-12-31 13:28:19 +0100

    Added version number to MANIFEST

---
+++ src/main/java/de/oglimmer/dsa/DSATalentTestCalcVersion.java
@@ -0,0 +1,40 @@
+package de.oglimmer.dsa;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import lombok.Getter;
+
+import com.jcabi.manifests.Manifests;
+
+public enum DSATalentTestCalcVersion {
+	INSTANCE;
+
+	private final String prefix = "DSA-TalCalc";
+
+	@Getter
+	private String version;
+	@Getter
+	private String commit;
+	@Getter
+	private String dsaCalcVersion;
+	@Getter
+	private String creationDate;
+
+	private DSATalentTestCalcVersion() {
+
+		try {
+			commit = Manifests.read(prefix + "-Git-Commit");
+			long time = Long.parseLong(Manifests.read(prefix + "-Creation-Date"));
+			creationDate = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(new Date(time));
+			dsaCalcVersion = Manifests.read(prefix + "-Version");
+		} catch (NumberFormatException e) {
+			commit = "?";
+			creationDate = "?";
+			dsaCalcVersion = "?";
+		}
+
+		version = "V" + dsaCalcVersion + " [Commit#" + commit + "] build " + creationDate;
+	}
+
+}
